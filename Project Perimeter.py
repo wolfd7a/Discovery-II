73677902f9b64b789433cf2cb3000ff4e1a2d582
wolfd7a <39986802+wolfd7a@users.noreply.github.com>
2018-06-06 13:56:25 +0200

    Perimeter II
    - Added user input to restart
    - Added few other possibilities
    - Added error case

--- Project Perimeter.py
+++ Project Perimeter.py
@@ -5,11 +5,36 @@ def circle(r) :
 def square(x,y) :
     square = x*y*2
     print(square)
-form = str(input('What object do you want the perimeter of : '))
-if (form == str('square')):
-    x = complex(input('Enter Length : '))
-    y = complex(input('Enter Width : '))
-    square (x,y)
-else :
-    r = complex(input('Enter Ray : '))
-    circle(2)
+def diamond(s) :
+    diamond = s*4
+    print(diamond)
+def pentagone(s) :
+    pentagone = s*5
+    print(pentagone)
+def hexagone(s) :
+    hexagone = s*5
+    print(hexagone)
+
+while True:
+    form = str(input('What object do you want the perimeter of : '))
+    if (form == str('square')):
+        x = float(input('Enter Length : '))
+        y = float(input('Enter Width : '))
+        square(x, y)
+    elif (form == str('diamond')):
+        s = float(input('Enter Side Length : '))
+        diamond(s)
+    elif (form == str('pentagone')):
+        s = float(input('Enter Side Length : '))
+        pentagone(s)
+    elif (form == str('hexagone')):
+        s = float(input('Enter Side Length : '))
+        hexagone(s)
+    elif (form == str('circle')):
+        r = float(input('Enter Ray : '))
+        circle(r)
+    else :
+        print("This form doesn't exist")
+    restart = str(input('Do you want to calculate an other form perimeter : '))
+    if restart == 'No' :
+        break
